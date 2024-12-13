2228f61a73e0abc4b4aefb6eb166248498602584
tanselbay1 <tanselbay1@hotmail.com>
2024-12-13 16:14:50 +0700

    start readline

--- main.c
+++ main.c
@@ -6,37 +6,55 @@
 /*   By: tanselbay1 <tanselbay1@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 17:52:19 by tanselbay1        #+#    #+#             */
-/*   Updated: 2024/11/30 17:58:41 by tanselbay1       ###   ########.fr       */
+/*   Updated: 2024/12/12 16:36:16 by tanselbay1       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-void lsh_loop(void)
+char *read_line(char *line)
+{
+    char *new_line;
+    
+    new_line = readline(line);
+    if (!new_line)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s\n", new_line);
+    return (new_line);
+}
+
+void lsh_loop(char *av)
 {
   char *line;
-  char **args;
-  int status;
+  //char **args;
+  //int status;
 
     printf("> ");
     // TODO: Read a line of input
-    line = lsh_read_line();
+    line = read_line(av);
     // TODO: Parse the line into arguments
-    args = lsh_split_line(line);
+    //args = lsh_split_line(line);
     // TODO: Execute the command
-    status = lsh_execute(args);
+    //status = lsh_execute(args);
 
     // TODO: Free the line and arguments
     free(line);
-    free(args);
+    //free(args);
 }
 
 int main(int ac, char **av)
 {
     if (ac == 2)
     {
-       lsh_loop();
+       lsh_loop(av[1]);
     }
-    printf('\n');
+    printf("\n");
     return (0);
 }
