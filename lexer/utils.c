d67b5e5968499ec0009a36967ab511b972ddd569
tanselbay1 <tanselbay1@hotmail.com>
2025-03-11 11:44:22 +0700

    Add is_special and is_space helper functions

---
+++ lexer/utils.c
@@ -0,0 +1,22 @@
+#include "minishell.h"
+
+
+char	*ft_getcwd(void)
+{
+	char	*path;
+
+	path = getcwd(NULL, 0);
+	if (path == NULL)
+		return (NULL);
+	return (path);
+}
+
+int	is_special(char c)
+{
+	return (c == '|' || c == '<' || c == '>' || c == '\'' || c == '"');
+}
+
+int	is_space(char c)
+{
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r');
+}
